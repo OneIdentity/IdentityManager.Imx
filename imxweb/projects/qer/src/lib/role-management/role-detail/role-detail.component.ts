20c08f6ffad9ae362e1a1d255ffbadf6bb0509a9
Hanno Bunjes <hanno.bunjes@quest.com>
2022-09-14 11:42:03 +0200

    Cumulative update v82 (#52)
    Cumulative update with v82 bug fixes

--- imxweb/projects/qer/src/lib/role-management/role-detail/role-detail.component.ts
+++ imxweb/projects/qer/src/lib/role-management/role-detail/role-detail.component.ts
@@ -24,24 +24,27 @@
  *
  */
 
-import { Component, Inject, Injector, OnInit, ViewChild } from '@angular/core';
+import { Component, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { EuiSidesheetRef, EUI_SIDESHEET_DATA } from '@elemental-ui/core';
 import { OwnershipInformation } from 'imx-api-qer';
 import { IEntity } from 'imx-qbm-dbts';
 import { RoleService } from '../role.service';
-import { ConfirmationService, ExtService, TabControlHelper, TabItem } from 'qbm';
+import { ConfirmationService, ExtService, TabItem } from 'qbm';
 import { MatTabGroup } from '@angular/material/tabs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'imx-role-detail',
   templateUrl: './role-detail.component.html',
   styleUrls: ['./role-detail.component.scss'],
 })
-export class RoleDetailComponent implements OnInit {
+export class RoleDetailComponent implements OnInit, OnDestroy {
   @ViewChild('tabs') public tabs: MatTabGroup;
   public dynamicTabs: TabItem[] = [];
   public parameters: { tablename: string; entity: IEntity };
+  public subscriptions$: Subscription[] = [];
 
+  private defaultClickHandler: Function;
   private canClose = true;
   private autoMembershipsValid = true;
   private confirmIsOpen = false;
@@ -58,7 +61,7 @@ export class RoleDetailComponent implements OnInit {
     private readonly roleService: RoleService,
     private readonly membershipService: RoleService,
     private readonly confirm: ConfirmationService,
-    private readonly tabService: ExtService
+    private readonly tabService: ExtService,
   ) {
 
     this.parameters = {
@@ -89,16 +92,34 @@ export class RoleDetailComponent implements OnInit {
   }
 
   public async ngOnInit(): Promise<void> {
-    /**
-     * Resolve an issue where the mat-tab navigation arrows could appear on first load
-     */
-    setTimeout(() => {
-      TabControlHelper.triggerResizeEvent();
-    });
     this.dynamicTabs = (await this.tabService.getFittingComponents<TabItem>('roleOverview',
       (ext) =>  ext.inputData.checkVisibility(this.parameters)))
       .sort((tab1: TabItem, tab2: TabItem) => tab1.sortOrder - tab2.sortOrder);
 
+    // implement tab checking logic
+    this.defaultClickHandler = this.tabs._handleClick;
+    this.tabs._handleClick = async (tab, header, index) => {
+      if (index !== this.tabs.selectedIndex && await this.checkConfirmation()) {
+        this.roleService.autoMembershipDirty(false);
+        this.roleService.dataDirty(false);
+        this.defaultClickHandler.apply(this.tabs, [tab, header, index])
+      }
+    }
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions$.map(sub => sub.unsubscribe());
+  }
+
+  public async checkConfirmation(): Promise<boolean> {
+    // Check if we leave maindata or membership with dirty data, assumes position of 0 and 1 resp.
+    const leavingMainWithDirty = this.tabs.selectedIndex === 0 && !this.canClose;
+    const leavingMemWithDirty = this.tabs.selectedIndex === 1 && !this.autoMembershipsValid;
+    let resp = true;
+    if (leavingMainWithDirty || leavingMemWithDirty) {
+      resp = await this.confirm.confirmLeaveWithUnsavedChanges(null, null, true);
+    }
+    return resp;
   }
 
   public canHaveMemberships(): boolean {
