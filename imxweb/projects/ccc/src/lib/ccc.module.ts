5cffe6a0c89646b49ebd3505afe0f9ffb5e69388
Jonas Drum <jonas.drum@igf-logic.com>
2023-05-31 15:36:26 +0200

    Fix Config

--- imxweb/projects/ccc/src/lib/ccc.module.ts
+++ imxweb/projects/ccc/src/lib/ccc.module.ts
@@ -1,16 +1,44 @@
 import { NgModule } from '@angular/core';
-import { CccComponent } from './ccc.component';
+import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import {ClassloggerService, QbmModule, RouteGuardService} from 'qbm';
+// import { IdentityComponent } from './identity/identity.component';
+import { CccService } from './ccc.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import {ReactiveFormsModule} from '@angular/forms';
 
-
+const routes: Routes = [
+//   {
+//     path: 'identity',
+//     component: IdentityComponent,
+//     canActivate: [RouteGuardService],
+//     resolve: [RouteGuardService]
+//   }
+];
 
 @NgModule({
   declarations: [
-    CccComponent
+    // IdentityComponent
   ],
   imports: [
-  ],
-  exports: [
-    CccComponent
+    CommonModule,
+    MatIconModule,
+    MatListModule,
+    MatCardModule,
+    TranslateModule,
+    RouterModule.forChild(routes),
+    ReactiveFormsModule,
+    QbmModule
   ]
 })
-export class CccModule { }
+export class CccModule {
+  constructor(
+    private readonly initializer: CccService, private readonly logger: ClassloggerService) {
+    this.logger.info(this, '🔥 CCC loaded');
+    this.initializer.onInit(routes);
+    this.logger.info(this, '▶️ CCC initialized');
+  }
+}
