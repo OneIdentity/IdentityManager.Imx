728e35bb92d10c75981985daf42a223450439908
peter.muehlhoefer <peter.muehlhoefer@condor.com>
2023-05-31 20:58:55 +0200

    refactor

--- imxweb/projects/ccc/src/lib/ccc-init.service.ts
+++ imxweb/projects/ccc/src/lib/ccc-init.service.ts
@@ -10,6 +10,9 @@ import {OwnershipInformation} from 'imx-api-qer';
   providedIn: 'root'
 })
 export class CccInitService {
+
+  private ownerships: OwnershipInformation[] = [];
+
   constructor(
     private readonly extService: ExtService,
     private readonly router: Router,
@@ -22,20 +25,22 @@ export class CccInitService {
     private readonly authService: AuthenticationService) {
   }
 
+
+
   public async onInit(routes: Route[]): Promise<void> {
     this.addRoutes(routes);
     // Ist notwenig sonst ist ownerships leer
     // TODO: Führt zu leichen wenn zwei user mit unterschiedlichen rechten sich nacheinander an/ab melden
     this.authService.onSessionResponse.subscribe(async session => {
+      console.log('ccc-session change', session?.IsLoggedIn ? 'login' :  session?.IsLoggedOut ? 'logout' : '???')
       if (session.IsLoggedIn) {
-
-        const ownerships = (await this.userModelService.getUserConfig()).Ownerships;
-        this.setupMenu(ownerships);
+        this.ownerships = (await this.userModelService.getUserConfig()).Ownerships;
       }
       if (session.IsLoggedOut){
-        this.menuService.clearFactories();
+        this.ownerships = [];
       }
     });
+    this.setupMenu();
   }
 
   public async createIdentity(): Promise<void> {
@@ -63,126 +68,133 @@ export class CccInitService {
     this.router.resetConfig(config);
   }
 
-  private setupMenu(ownerships: OwnershipInformation[]): void {
+  private setupMenu(): void {
     this.menuService.addMenuFactories(
-      (preProps: string[], groups: string[]) => {
-        if (!preProps.includes('ITSHOP')) {
-          return null;
-        }
-        const isCreator = this.isPersonCreator(groups);
-        const isManager = this.isPersonManager(groups);
-        const isContactCenterAgentMgr = this.isContactCenterAgentManager(groups);
-
-        if (!isCreator && !isManager) {
-          return null;
-        }
-
-        const items: MenuItem[] = [
-          {
-            id: 'ATT_NewIdentity',
-            trigger: () => this.createIdentity(),
-            title: '#LDS#Heading Create Identity',
-            sorting: '20-10',
-          }
-        ];
-
-        if (isManager) {
-          items.push(
-            {
-              id: 'ATT_Identities',
-              navigationCommands: {commands: ['resp', 'identities']},
-              title: '#LDS#Heading My Direct Reports',
-              sorting: '20-20',
-            }
-          );
-        }
-
-        if (isContactCenterAgentMgr) {
-          items.push(
-            {
-              id: 'CCC_Identity',
-              route: 'identity',
-              title: '#LDS#Heading Import Identities',
-              sorting: '20-30',
-            }
-          );
-        }
-
-        const menu: MenuItem = {
-          id: 'CCC_Identities',
-          title: '#LDS#Identities',
-          sorting: '20',
-          items
-        };
-        return menu;
-      },
-      (preProps: string[]) => {
-
-        if (!preProps.includes('ITSHOP')) {
-          return null;
-        }
-
-        if (ownerships.length < 1) {
-          return null;
-        }
-
-        const items: MenuItem[] = [];
-        // TODO: Menüwechsel untereinander kaputt (Problem liegt an gleicher Komponente (role service) nicht hier!)
-        // https://stackoverflow.com/questions/47813927/how-to-refresh-a-component-in-angular
-        if (this.isOwnerOf(ownerships, 'Department')) {
-          items.push(
-            {
-              id: 'CCC_Department',
-              navigationCommands: {commands: ['resp', 'Department']},
-              title: '#LDS#Heading My Departments',
-              sorting: '30-60',
-            },
-          );
-        }
-
-        if (this.isOwnerOf(ownerships, 'Locality')) {
-          items.push(
-            {
-              id: 'CCC_Locality',
-              navigationCommands: {commands: ['resp', 'Locality']},
-              title: '#LDS#Heading My Localities',
-              sorting: '30-70',
-            },
-          );
-        }
-
-        if (this.isOwnerOf(ownerships, 'ProfitCenter')) {
-          items.push(
-            {
-              id: 'CCC_ProfitCenter',
-              navigationCommands: {commands: ['resp', 'ProfitCenter']},
-              title: '#LDS#Heading My Cost Centres',
-              sorting: '30-80',
-            },
-          );
-        }
-
-        if (this.isOwnerOf(ownerships, 'AERole')) {
-          items.push(
-            {
-              id: 'CCC_AERole',
-              navigationCommands: {commands: ['resp', 'AERole']},
-              title: '#LDS#Heading My AERoles',
-              sorting: '30-90',
-            },
-          );
-        }
-        if (items.length === 0) {
-          return null;
-        }
-        return {
-          id: 'ROOT_Responsibilities',
-          title: '#LDS#Responsibilities',
-          sorting: '30',
-          items
-        };
-      });
+      (preProps: string[], groups: string[]) => this.getCreateIdentityMenuItems(preProps,groups),
+      (preProps: string[]) => this.getOwnershipMenuItems(preProps));
   }
+
+  private getCreateIdentityMenuItems (preProps: string[], groups: string[]): MenuItem {
+    if (!preProps.includes('ITSHOP')) {
+      return null;
+    }
+    const isCreator = this.isPersonCreator(groups);
+    const isManager = this.isPersonManager(groups);
+    const isContactCenterAgentMgr = this.isContactCenterAgentManager(groups);
+
+    if (!isCreator && !isManager) {
+      return null;
+    }
+
+    const items: MenuItem[] = [
+      {
+        id: 'ATT_NewIdentity',
+        trigger: () => this.createIdentity(),
+        title: '#LDS#Heading Create Identity',
+        sorting: '20-10',
+      }
+    ];
+
+    if (isManager) {
+      items.push(
+        {
+          id: 'ATT_Identities',
+          navigationCommands: {commands: ['resp', 'identities']},
+          title: '#LDS#Heading My Direct Reports',
+          sorting: '20-20',
+        }
+      );
+    }
+
+    if (isContactCenterAgentMgr) {
+      items.push(
+        {
+          id: 'CCC_Identity',
+          route: 'identity',
+          title: '#LDS#Heading Import Identities',
+          sorting: '20-30',
+        }
+      );
+    }
+
+    const menuItem: MenuItem = {
+      id: 'CCC_Identities',
+      title: '#LDS#Identities',
+      sorting: '20',
+      items
+    };
+    return menuItem;
+  }
+
+  private getOwnershipMenuItems(preProps: string[]): MenuItem
+    {
+
+      if (!preProps.includes('ITSHOP')) {
+        return null;
+      }
+
+      if (this.ownerships.length < 1) {
+        return null;
+      }
+
+      const items: MenuItem[] = [];
+      // TODO: Menüwechsel untereinander kaputt (Problem liegt an gleicher Komponente (role service) nicht hier!)
+      // https://stackoverflow.com/questions/47813927/how-to-refresh-a-component-in-angular
+      if (this.isOwnerOf(this.ownerships, 'Department')) {
+        items.push(
+          {
+            id: 'CCC_Department',
+            navigationCommands: {commands: ['resp', 'Department']},
+            title: '#LDS#Heading My Departments',
+            sorting: '30-60',
+          },
+        );
+      }
+
+      if (this.isOwnerOf(this.ownerships, 'Locality')) {
+        items.push(
+          {
+            id: 'CCC_Locality',
+            navigationCommands: {commands: ['resp', 'Locality']},
+            title: '#LDS#Heading My Localities',
+            sorting: '30-70',
+          },
+        );
+      }
+
+      if (this.isOwnerOf(this.ownerships, 'ProfitCenter')) {
+        items.push(
+          {
+            id: 'CCC_ProfitCenter',
+            navigationCommands: {commands: ['resp', 'ProfitCenter']},
+            title: '#LDS#Heading My Cost Centres',
+            sorting: '30-80',
+          },
+        );
+      }
+
+      if (this.isOwnerOf(this.ownerships, 'AERole')) {
+        items.push(
+          {
+            id: 'CCC_AERole',
+            navigationCommands: {commands: ['resp', 'AERole']},
+            title: '#LDS#Heading My AERoles',
+            sorting: '30-90',
+          },
+        );
+      }
+      if (items.length === 0) {
+        return null;
+      }
+      const menuItem: MenuItem = {
+        id: 'ROOT_Responsibilities',
+        title: '#LDS#Responsibilities',
+        sorting: '30',
+        items
+      };
+      return menuItem;
+  }
+
 
   private isPersonManager(groups: string[]): boolean {
     return groups.find(item => item.toUpperCase() === 'VI_4_ALLMANAGER') != null;
