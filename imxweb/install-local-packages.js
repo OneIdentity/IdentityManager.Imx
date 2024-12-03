0fe5797c643fcaacbfd1c2e7d6050379bc7b2560
PROD\CGriffit <cody.griffith@quest.com>
2024-12-03 13:20:31 +0100

    Switch to only ubuntu on github agents

--- imxweb/install-local-packages.js
+++ imxweb/install-local-packages.js
@@ -1,10 +1,12 @@
 const child_process = require('child_process');
 const fs = require('fs');
-const path = require('path');
+const path = require('upath');
+
 const readline = require('readline').createInterface({ input: process.stdin, output: process.stdout });
 
 // --skip-dialog will auto overwrite feeds if imx-modules are present
 
+const isWin = process.platform === 'win32';
 const imxDir = 'imx-modules';
 const question = (str) => new Promise((resolve) => readline.question(str, resolve));
 const steps = {
@@ -38,30 +40,30 @@ steps.start();
 function overwrite() {
   console.log('Overwriting with...');
   let installArg = '';
+  let filePath;
   fs.readdirSync(imxDir)
     .filter((file) => file.endsWith('.tgz'))
     .forEach((file) => {
+      filePath = isWin ? path.join(imxDir, file) : path.join(__dirname, imxDir, file);
       if (file.includes('imx-')) {
-        const filePath = path.join(imxDir, file);
         const baseName = path.parse(file).name;
-        installArg += '@' + imxDir + '/' + baseName + '@' + filePath + ' ';
-        console.log(filePath);
+        installArg += ['@', path.join(imxDir, baseName), '@', filePath, ' '].join('');
       } else if (file.includes('cadence-icon')) {
-        const filePath = path.join(imxDir, file);
-        installArg += '@elemental-ui/cadence-icon' + '@' + filePath + ' ';
-        console.log(filePath);
+        installArg += ['@elemental-ui/cadence-icon@', filePath, ' '].join('');
       } else if (file.includes('core')) {
-        const filePath = path.join(imxDir, file);
-        installArg += '@elemental-ui/core' + '@' + filePath + ' ';
-        console.log(filePath);
+        installArg += ['@elemental-ui/core@', filePath, ' '].join('');
       }
+      console.log(filePath);
     });
 
-  const child = child_process.spawnSync('npm', ['i', installArg, '--save=false'], { encoding: 'utf8', shell: true });
+  const child = child_process.spawnSync('npm', ['i', installArg, '--save=false'], {
+    encoding: 'utf8',
+    shell: true,
+  });
   if (child.status === 0) {
     console.log('Overwrite Finished');
   } else {
-    console.log('There was an error:')
+    console.log('There was an error:');
     console.error(child.output);
     process.exit(1);
   }
