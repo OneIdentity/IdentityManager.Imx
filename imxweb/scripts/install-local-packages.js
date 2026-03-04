8f9f778c80673d04a13e9633d70e7658fb5fc85b
Martina Graeber (mgraeber) <martina.graeber@oneidentity.com>
2026-03-04 17:22:40 +0100

    miss revert

--- imxweb/scripts/install-local-packages.js
+++ imxweb/scripts/install-local-packages.js
@@ -34,8 +34,8 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g = Object.create((typeof Iterator === "function" ? Iterator : Object).prototype);
-    return g.next = verb(0), g["throw"] = verb(1), g["return"] = verb(2), typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
     function verb(n) { return function (v) { return step([n, v]); }; }
     function step(op) {
         if (f) throw new TypeError("Generator is already executing.");
@@ -74,7 +74,6 @@ var process = require("process");
 var readline = require("readline");
 // Look for a positional argument to point where the imx-modules are; defaults to imx-modules
 var imxModuleDir = process.argv.length > 2 ? process.argv[2] : 'imx-modules';
-var isWin = process.platform === 'win32';
 var nodePackageDir = '@imx-modules';
 var nodeElementalDir = '@elemental-ui';
 var nodeModuleDir = 'node_modules';
@@ -139,7 +138,7 @@ function overwrite() {
     fs.readdirSync(imxModuleDir)
         .filter(function (file) { return file.endsWith('.tgz'); })
         .forEach(function (file) {
-        filePath = isWin ? path.join(imxModuleDir, file) : path.join(__dirname, imxModuleDir, file);
+        filePath = path.resolve(imxModuleDir, file);
         if (file.includes('imx-')) {
             var baseName = path.parse(file).name;
             installArg += [nodePackageDir, '/', baseName, '@', filePath, ' '].join('');
