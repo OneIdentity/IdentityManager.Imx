3f7c8a6a497a33e43609b7c0e8bd1e12702bd60e
AbanoubNassem1D <Abanoub.Nassem@1dconsulting.com>
2026-07-15 12:42:53 +0200

    baseurle

--- imxweb/projects/qer-app-portal/src/environments/environment.ts
+++ imxweb/projects/qer-app-portal/src/environments/environment.ts
@@ -30,7 +30,7 @@
 
 export const environment = {
   production: false,
-  clientUrl: 'http://localhost:8182',
+  clientUrl: 'http://localhost/ApiServer',
   appName: 'qer-app-portal',
   appVersion: '1.0.0'
 };
